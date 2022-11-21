6537e888344f930bfc53f6ce903e0d7197e64931
Andrey <basmsk@mail.ru>
2022-11-21 20:32:09 +0500

    the importance of return

--- script.js
+++ script.js
@@ -10,5 +10,21 @@ function convert(amount, curr) {
 }
 
 function promotion(result) {
-    console.log(result * discount)
+    console.log(result * discount);
 }
+
+const res = convert(500, usdCurr);
+promotion(res);
+
+function test() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+        if (i === 3) return
+    }
+    console.log('Done');
+}
+
+test();
+
+function doNothing() {};
+console.log(doNothing() === undefined);
